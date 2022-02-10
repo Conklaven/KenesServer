4a10da6282dcd3dffef0a6725446f5901187f595
Conklaven <jacob.klaven@gmail.com>
2022-02-10 14:58:48 +0200

    remove ssl

--- config/Database.js
+++ config/Database.js
@@ -16,6 +16,8 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_
           rejectUnauthorized: false
         }
       }
+    })
+db
   .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.');
@@ -23,6 +25,5 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   })
-})
 
 export default db
