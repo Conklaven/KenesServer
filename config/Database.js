288a4bc14d9b7d4bfefd48fc41b26621c7820d03
Conklaven <jacob.klaven@gmail.com>
2022-02-10 15:02:18 +0200

    remove ssl

--- config/Database.js
+++ config/Database.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
-    host:'localhost',
+    host:"127.0.0.1",
     dialect:'postgres',
     logging: true,
     dialectOptions: {
