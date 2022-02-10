14f834fd73bd5b54ad5f7fcd1b8eb778be24eb68
Conklaven <jacob.klaven@gmail.com>
2022-02-10 15:17:06 +0200

    remove ssl

--- config/Database.js
+++ config/Database.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS, {
-    host:"127.0.0.1",
+const db = new Sequelize(process.env.DATABASE_URL, {
+    host:"localhost",
     dialect:'postgres',
     logging: true,
     dialectOptions: {
