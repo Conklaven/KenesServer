e197c0683d6ae0ce4045fcb11ca27329b46152ae
Conklaven <jacob.klaven@gmail.com>
2022-02-15 11:46:02 +0200

    cors

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ try {
 }
 
 
-app.use(cors({credentials: true, origin:'https://kenes-tours.netlify.app'}));
+// app.use(cors({credentials: true, origin:'https://kenes-tours.netlify.app'}));
+app.use(cors(corsOptions))
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
 app.use(cookieParser());
 app.use(express.json());
 app.use(router)
